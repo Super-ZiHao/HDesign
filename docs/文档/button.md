42e8139121b909a5f7e05000889bdc467103eba0
v_hzihhuang <zihao@zihaodemacdeMacBook-Pro.local>
2022-08-31 22:26:32 +0800

    perf: Button 按钮组件类型

--- docs/文档/button.md
+++ docs/文档/button.md
@@ -9,32 +9,56 @@ export default () => {
       <h3>基本用法</h3>
       <div style={{ display: 'flex', gap: 8 }}>
         <Button animation>Default</Button>
-        <Button type="primary" animation>Primary</Button>
-        <Button type="success" animation>Success</Button>
-        <Button type="info" animation>Info</Button>
-        <Button type="warning" animation>Warning</Button>
-        <Button type="danger" animation>Danger</Button>
+        <Button type="primary" animation>
+          Primary
+        </Button>
+        <Button type="success" animation>
+          Success
+        </Button>
+        <Button type="warning" animation>
+          Warning
+        </Button>
+        <Button type="danger" animation>
+          Danger
+        </Button>
       </div>
+
       <h3>轻量</h3>
       <div style={{ display: 'flex', gap: 8 }}>
-        <Button animation>Default</Button>
-        <Button type="primary" disabled animation>Primary</Button>
-        <Button type="success" disabled>Success</Button>
-        <Button type="info" animation>Info</Button>
-        <Button type="warning" animation>Warning</Button>
-        <Button type="danger" animation>Danger</Button>
+        <Button plain>Default</Button>
+        <Button type="primary" plain>
+          Primary
+        </Button>
+        <Button type="success" plain>
+          Success
+        </Button>
+        <Button type="warning" plain>
+          Warning
+        </Button>
+        <Button type="danger" plain>
+          Danger
+        </Button>
       </div>
+
       <h3>禁用</h3>
       <div style={{ display: 'flex', gap: 8 }}>
-        <Button animation>Default</Button>
-        <Button type="primary" disabled >Primary</Button>
-        <Button type="success" disabled>Success</Button>
-        <Button type="info" disabled>Info</Button>
-        <Button type="warning" disabled>Warning</Button>
-        <Button type="danger" disabled>Danger</Button>
+        <Button disabled>Default</Button>
+        <Button type="primary" disabled>
+          Primary
+        </Button>
+        <Button type="success" disabled>
+          Success
+        </Button>
+        <Button type="warning" disabled>
+          Warning
+        </Button>
+        <Button type="danger" disabled>
+          Danger
+        </Button>
       </div>
     </>
-  )
+  );
 };
 ```
 
+<API src="../../src/Button/index.tsx" />
--- src/Button/index.tsx
+++ src/Button/index.tsx
@@ -3,16 +3,34 @@ import './index.scss';
 
 export type Props = {
   className?: string;
-  style?: CSSProperties
+  style?: CSSProperties;
   children?: React.ReactNode;
-  /** 是否需要点击动画 */
-  animation?: boolean
-  /** 按钮类型 */
-  type?: 'primary' | 'success' | 'warning' | 'danger' | 'info'
-  /** 禁用 */
-  disabled?: boolean
-  onClick?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
-  onMouseDown?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
+  /**
+   * @description 是否需要点击动画
+   * @default false
+   */
+  animation?: boolean;
+  /**
+   * @description 按钮类型
+   * @default "defalut"
+   */
+  type?: 'default' | 'primary' | 'success' | 'warning' | 'danger';
+  /**
+   * @description 轻量按钮
+   * @default false
+   */
+  plain?: boolean;
+  /**
+   * @description 是否禁用
+   * @default false
+   */
+  disabled?: boolean;
+  /** 点击事件 */
+  onClick?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  /** 移入事件 */
+  onMouseOver?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  /** 移出事件 */
+  onMouseOut?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
 };
 
 const Button: React.FC<Props> = (props) => {
@@ -21,46 +39,53 @@ const Button: React.FC<Props> = (props) => {
     style,
     children,
     animation = false,
-    type = '',
+    type = 'default',
+    plain = false,
     disabled,
     onClick,
-    onMouseDown,
+    onMouseOver,
+    onMouseOut,
     ...resetProps
   } = props;
-  const buttonRef = useRef<HTMLButtonElement>(null)
+  const buttonRef = useRef<HTMLButtonElement>(null);
   const handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     onClick?.(e);
-  }
+  };
   const handleMouseDown = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    onMouseDown?.(e)
     // 动画操作
-    if (!animation || disabled) return
-    const button = (buttonRef.current as HTMLButtonElement);
+    if (!animation || disabled) return;
+    const button = buttonRef.current as HTMLButtonElement;
     const div = document.createElement('div');
     div.classList.add('hd-button-point');
     div.style.left = `${e.clientX - button.offsetLeft}px`;
     div.style.top = `${e.clientY - button.offsetTop}px`;
     (buttonRef.current as HTMLButtonElement).appendChild(div);
     const handleUp = () => {
-      div.classList.add('del')
+      div.classList.add('del');
       setTimeout(() => {
         div?.remove();
       }, 300);
       button.removeEventListener('mouseup', handleUp);
-    }
-    button.addEventListener('mouseup', handleUp)
-  }
-  return <button
-    ref={buttonRef}
-    className={`hd-button ${className} ${type} ${animation ? 'animation' : ''} ${disabled ? 'disabled' : ''}`}
-    style={{
-      ...style
-    }}
-    onClick={handleClick}
-    onMouseDown={handleMouseDown}
-    {...resetProps}
-  >
-    {animation ? <span className='hd-text'>{children}</span> : children}
-  </button>;
+    };
+    button.addEventListener('mouseup', handleUp);
+  };
+  return (
+    <button
+      ref={buttonRef}
+      className={`hd-button ${className} ${type} ${animation ? 'animation' : ''} ${
+        disabled ? 'disabled' : ''
+      } ${plain ? 'plain' : ''}`}
+      style={{
+        ...style,
+      }}
+      onClick={handleClick}
+      onMouseDown={handleMouseDown}
+      onMouseLeave={onMouseOut}
+      onMouseEnter={onMouseOver}
+      {...resetProps}
+    >
+      {animation ? <span className="hd-text">{children}</span> : children}
+    </button>
+  );
 };
 export default Button;
